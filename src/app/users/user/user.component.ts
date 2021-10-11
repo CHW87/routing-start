0e0678a8529be6169eced391d9b7cebf6afec7f2
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-10-11 14:26:17 +0300

    An Important Note about Route Observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number, name: string };
+  paramsSubscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    this.route.params.subscribe(
+    // ------- Use this approach to get informed about any changes in your route parameters------------//
+    // ------- If you are 100% sure that the route parameters will not change,dont subscribe,use snapshot method---//
+   this.paramsSubscription = this.route.params
+     .subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
@@ -24,4 +30,7 @@ export class UserComponent implements OnInit {
     );
   }
 
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
 }
